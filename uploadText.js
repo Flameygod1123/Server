402c95ee2d70a43ac8609f01d2194b7980466030
Flameygod1123 <149326067+Flameygod1123@users.noreply.github.com>
2023-11-19 17:14:38 -0500

    Update uploadText.js

--- uploadText.js
+++ uploadText.js
@@ -1,1 +1,12 @@
 
+// api/uploadText.js
+
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    const { text } = req.body;
+    console.log('Received text:', text);
+    res.status(200).json({ success: true, message: 'Text received successfully' });
+  } else {
+    res.status(405).json({ error: 'Method Not Allowed' });
+  }
+}
